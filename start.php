218b523c55ad7880fef9c1ca9e7b62c6217d6789
PremiumKita <67585207+PremiumKita@users.noreply.github.com>
2020-06-29 22:26:12 +0700

    Add files via upload

---
+++ start.php
@@ -0,0 +1,55 @@
+<?php  
+
+function get($url = null, $headers = null) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	if ($headers != "") {
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	}
+
+	return $result = curl_exec($ch);
+	curl_close($ch);
+}
+
+function number($length) {
+	$characters = '0123456789';
+	$charactersLength = strlen($characters);
+	$randomString = '';
+	for ($i = 0; $i < $length; $i++) {
+		$randomString .= $characters[rand(0, $charactersLength - 1)];
+	}
+	return $randomString;
+}
+
+function create_account() {
+	
+}
+
+echo "\e[96m---------------------------\n";
+echo "|                         |\n";
+echo "|     Netflix Premium     |\n";
+echo "|       PremiumKita       |\n";
+echo "|                         |\n";
+echo "---------------------------\n";
+
+
+echo "\e[93mJumlah Akun : ";
+$banyak = trim(fgets(STDIN));
+echo "\n";
+echo "\e[93mSEDANG MEMBUAT AKUN\n";
+
+for ($i = 0; $i < $banyak ; $i++) {
+	sleep(3);
+	flush();
+	echo $data = "\e[92m[SUKSES]\e[0m premiumkita".number(6)."@gmail.com | premiumkita123 [Expired ".date('d M Y', strtotime('+30 days', strtotime(date('d M Y'))))."]\n\r";
+	$fh = fopen("result.txt", "a");
+	fwrite($fh, "premiumkita".number(6)."@gmail.com | premiumkita123 [Expired ".date('d M Y', strtotime('+30 days', strtotime(date('d M Y'))))."] \n\r");
+	fclose($fh);
+}
+echo "====================================================\n";
+echo "Akun Netflix Premium \e[92mSUKSES\e[0m Dibuat Cek result.txt\n";
+echo "====================================================\n";
+
+?>
